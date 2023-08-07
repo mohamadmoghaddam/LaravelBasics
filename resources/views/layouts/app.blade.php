73bf0f146bfdffccf828535679b27882d424432c
mohamadreza <mohamadreza.ghasemi1383@gmail.com>
2023-08-07 18:17:10 +0430

    new file:   app/Http/Controllers/MainController.php 	new file:   resources/views/layouts/app.blade.php 	new file:   resources/views/mainpage.blade.php 	modified:   routes/web.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- HTML Codes by Quackit.com -->
+<title>
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {background-color:#c52b2b;background-repeat:repeat;background-position:top left;background-attachment:fixed;}
+h1{font-family:Impact, sans-serif;color:#ffffff;background-color:#c52b2b;}
+p {text-align:center;font-family:Georgia, serif;font-size:14px;font-style:italic;font-weight:normal;color:#c52b2b;background-color:#ffffff;}
+</style>
+</head>
+<body>
+
+@yield('main')
+
+
+</body>
+</html>
---
+++ resources/views/mainpage.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('main')
+<h1>this the main page of our site</h1>
+<p>this the main page of our site ssssssssssssssssssssssssssssss</p>
+@endsection
